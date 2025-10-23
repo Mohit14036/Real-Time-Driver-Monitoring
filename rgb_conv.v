6b7773d87be9631fa73be6225983beb6d4302c3b
Vibushita S <86812991+Vibushita@users.noreply.github.com>
2025-10-23 15:18:11 +0530

    Update rgb_conv.v

--- rgb_conv.v
+++ rgb_conv.v
@@ -4,33 +4,33 @@ module rgb_conv #(
     parameter DATA_WIDTH = 8,
     parameter KERNEL_SIZE = 10
 )(
-    input wire clk,
-    input wire rst,
-    input wire load_weight,
-    input wire start_conv,
+    (* keep = "true" *) input wire clk,
+    (* keep = "true" *) input wire rst,
+    (* keep = "true" *) input wire load_weight,
+    (* keep = "true" *) input wire start_conv,
 
     // Each color has 3x1 inputs per clock (3 rows)
-    input wire [KERNEL_SIZE*KERNEL_SIZE*DATA_WIDTH-1:0] input_win_r,
-    input wire [KERNEL_SIZE*KERNEL_SIZE*DATA_WIDTH-1:0] input_win_g,
-    input wire [KERNEL_SIZE*KERNEL_SIZE*DATA_WIDTH-1:0] input_win_b,
+    (* keep = "true" *) input wire [KERNEL_SIZE*KERNEL_SIZE*DATA_WIDTH-1:0] input_win_r,
+    (* keep = "true" *) input wire [KERNEL_SIZE*KERNEL_SIZE*DATA_WIDTH-1:0] input_win_g,
+    (* keep = "true" *) input wire [KERNEL_SIZE*KERNEL_SIZE*DATA_WIDTH-1:0] input_win_b,
 
     // Each color has its own 3x3 kernel
-    input wire [KERNEL_SIZE*KERNEL_SIZE*DATA_WIDTH-1:0] weights_r,
-    input wire [KERNEL_SIZE*KERNEL_SIZE*DATA_WIDTH-1:0] weights_g,
-    input wire [KERNEL_SIZE*KERNEL_SIZE*DATA_WIDTH-1:0] weights_b,
+    (* keep = "true" *) input wire [KERNEL_SIZE*KERNEL_SIZE*DATA_WIDTH-1:0] weights_r,
+    (* keep = "true" *) input wire [KERNEL_SIZE*KERNEL_SIZE*DATA_WIDTH-1:0] weights_g,
+    (* keep = "true" *) input wire [KERNEL_SIZE*KERNEL_SIZE*DATA_WIDTH-1:0] weights_b,
 
-    output reg [(2*DATA_WIDTH+9)-1:0] conv_outs_rgb,
-    output reg start_fifo
+    (* keep = "true" *) output reg [(2*DATA_WIDTH+8):0] conv_outs_rgb,
+    (* keep = "true" *) output reg start_fifo
     // Final combined convolution output
     
 );
 
-     wire [2*DATA_WIDTH+6:0] conv_r;
-     wire [2*DATA_WIDTH+6:0] conv_g;
-     wire [2*DATA_WIDTH+6:0] conv_b;
+     (* keep = "true" *) wire [2*DATA_WIDTH+6:0] conv_r;
+     (* keep = "true" *) wire [2*DATA_WIDTH+6:0] conv_g;
+     (* keep = "true" *) wire [2*DATA_WIDTH+6:0] conv_b;
 
     // Instantiate systolic array for Red channel
-    conv #(.DATA_WIDTH(DATA_WIDTH), .KERNEL_SIZE(10)) red_array (
+    (* dont_touch = "true" *) conv #(.DATA_WIDTH(DATA_WIDTH), .KERNEL_SIZE(KERNEL_SIZE)) red_array (
         .clk(clk),
         .rst(rst),
         .load_weight(load_weight),
@@ -41,7 +41,7 @@ module rgb_conv #(
     );
 
     // Instantiate systolic array for Green channel
-    conv #(.DATA_WIDTH(DATA_WIDTH), .KERNEL_SIZE(10)) green_array (
+    (* dont_touch = "true" *) conv #(.DATA_WIDTH(DATA_WIDTH), .KERNEL_SIZE(KERNEL_SIZE)) green_array (
         .clk(clk),
         .rst(rst),
         .load_weight(load_weight),
@@ -52,7 +52,7 @@ module rgb_conv #(
     );
 
     // Instantiate systolic array for Blue channel
-    conv #(.DATA_WIDTH(DATA_WIDTH), .KERNEL_SIZE(10)) blue_array (
+    (* dont_touch = "true" *) conv #(.DATA_WIDTH(DATA_WIDTH), .KERNEL_SIZE(KERNEL_SIZE)) blue_array (
         .clk(clk),
         .rst(rst),
         .load_weight(load_weight),
@@ -64,8 +64,11 @@ module rgb_conv #(
     
     always@ (posedge clk) begin 
         start_fifo <= start_conv;
+        if(rst) begin
+            conv_outs_rgb <= 0;
+        end
         
-        if(conv_valid_r && conv_valid_g && conv_valid_b) begin
+        else if(conv_valid_r && conv_valid_g && conv_valid_b) begin
         
             conv_outs_rgb[1*(2*DATA_WIDTH+8)-1 -: 2*DATA_WIDTH+8] <= conv_r + conv_g + conv_b;
           //  conv_outs_rgb[2*(2*DATA_WIDTH+6)-1 -: 2*DATA_WIDTH+6] <= conv_r + conv_g + conv_b;
