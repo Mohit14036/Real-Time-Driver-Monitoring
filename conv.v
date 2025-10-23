c8343bec1b0c20d1cde8ce4916680db19839d052
Vibushita S <86812991+Vibushita@users.noreply.github.com>
2025-10-23 15:18:23 +0530

    Update conv.v

--- conv.v
+++ conv.v
@@ -1,22 +1,22 @@
 `timescale 1ns / 1ps
 
-module conv #(
+(* use_dsp = "yes" *) module conv #(
     parameter DATA_WIDTH = 8,
     parameter KERNEL_SIZE = 10
 )(
-    input  wire clk,
-    input  wire rst,
-    input  wire load_weight,
+    (* keep = "true" *) input  wire clk,
+    (* keep = "true" *) input  wire rst,
+    (* keep = "true" *) input  wire load_weight,
     
-    input  wire [KERNEL_SIZE*KERNEL_SIZE*DATA_WIDTH-1:0] input_col,       // 3 values per clock: one column
-    input  wire [KERNEL_SIZE*KERNEL_SIZE*DATA_WIDTH-1:0] filter_weights,  // 3x3 kernel weights
+    (* keep = "true" *) input  wire [KERNEL_SIZE*KERNEL_SIZE*DATA_WIDTH-1:0] input_col,       // 3 values per clock: one column
+    (* keep = "true" *) input  wire [KERNEL_SIZE*KERNEL_SIZE*DATA_WIDTH-1:0] filter_weights,  // 3x3 kernel weights
 
-    output reg  [2*DATA_WIDTH+6:0] conv_out,        // final output
-    output reg conv_valid
+    (* keep = "true" *) output reg  [2*DATA_WIDTH+6:0] conv_out,        // final output
+    (* keep = "true" *) output reg conv_valid
 );
 
     // Weights (for now all 1)
-    wire [DATA_WIDTH-1:0] weights[0:KERNEL_SIZE*KERNEL_SIZE-1];
+    (* keep = "true" *) wire [DATA_WIDTH-1:0] weights[0:KERNEL_SIZE*KERNEL_SIZE-1];
     genvar i;
     generate
       for (i=0; i<KERNEL_SIZE*KERNEL_SIZE; i=i+1) begin
@@ -24,7 +24,7 @@ module conv #(
       end
     endgenerate
     
-    wire [DATA_WIDTH*2-1:0] products [0:KERNEL_SIZE*KERNEL_SIZE-1];
+    (* keep = "true" *) wire [DATA_WIDTH*2-1:0] products [0:KERNEL_SIZE*KERNEL_SIZE-1];
     genvar k;
     generate
         for (k=0; k<KERNEL_SIZE*KERNEL_SIZE; k=k+1) begin : MACS
@@ -32,7 +32,7 @@ module conv #(
         end
     endgenerate
     
-    integer m;
+   (* keep = "true" *)  integer m;
     
     always @(posedge clk or posedge rst) begin
         if (rst) begin
